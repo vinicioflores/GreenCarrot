e61eb72ebab12ea6cd7bbb288e58e2441ab52880
Flores <vinicio.flores@intel.com>
2018-09-19 11:25:47 -0600

    fixed bug in mongo routers compose bootstrap config - forgot bind_ip

--- app/python_exec/app.py
+++ app/python_exec/app.py
@@ -1,10 +1,11 @@
 from cassandra.cluster import Cluster
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from pymongo.errors import AutoReconnect
 
 import datetime
 
-#client = MongoClient('mongodb://router01:27028/')
+#client = MongoClient('router01',27028)
 client = MongoClient('mongodb://127.0.0.1:27028/')
 
 db = client.GreenCarrotRutasDB
@@ -14,6 +15,28 @@ deliveries=db.deliveries
 planrutas=db.planrutas
 routes=db.routes
 trucks=db.trucks
+
+
+def mongo_get_delivery(row):
+    truckid="507f191e810c19729de860ea"
+    data = {}
+    data['truckid'] = ObjectId(truckid)
+    data['name'] = "greenCarrot"
+    data['assignedRoute']= row.delivery_route
+    data['planned_position_order']= 3
+    data['planned_stop_name']= row.delivery_stop_in_route
+    data['planned_consumer_person']= row.username
+    data['product_name']= row.productname
+    data['product_family']= row.producttype
+    data['payment_from_consumer_in_spot']= row.pay_in_consumer_spot
+    data['payment_method']= row.payment_methods
+    data['payment_amount']= float(row.cost)
+    data['completed']=False
+    return data
+
+
+
+print(client)
 
 #cluster=Cluster(["inv01data"])
 cluster=Cluster(["localhost"])
@@ -25,9 +48,9 @@ lastLen = 1
 dataLen = 0
 
 while True:
-
     ## Poll for new incoming orders in Cassandra
     session = cluster.connect()
+    session.execute("use greencarrotinventoryreplicationstrategy")
     cnt=session.execute("""
     SELECT COUNT(*)  as cnt
     FROM items_ordered_to_deliver_to_consumers 
@@ -44,21 +67,20 @@ while True:
         ## add routes planning in MongoDB
         for row in rows:
             print(row)
-            data = {}
-            data['truckid'] = ObjectId("507f191e810c19729de860ea")
-            data['name'] = "greenCarrot"
-            data['assignedRoute']: row.delivery_route
-            data['planned_position_order']: 3
-            data['planned_stop_name']: row.delivery_stop_in_route
-            data['planned_consumer_person']: row.username
-            data['product_name']: row.productname
-            data['product_family']: row.producttype
-            data['payment_from_consumer_in_spot']: row.pay_in_consumer_spot
-            data['payment_method']: row.payment_methods
-            data['payment_amount']: row.cost
-            data['completed']:False
-            deliveries.insert_one(data)
-            
+            #refresh_mongo_connection()
+            try:
+                data = mongo_get_delivery(row)
+                deliveries.insert_one(data)
+            except AutoReconnect:
+                print("Unable to connect ... Refreshing connection ...")
+                #client = MongoClient('router02',27029)
+                client = MongoClient('mongodb://127.0.0.1:27029/')
+                db = client.GreenCarrotRutasDB
+                deliveries=db.deliveries
+                data = mongo_get_delivery(row)
+                deliveries.insert_one(data)
+                
     lastLen = dataLen
     
     
+
